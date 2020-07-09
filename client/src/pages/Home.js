83e0179934734ab2497f3f1a5aa90f39611f6326
Gautam Arora <gautamarora6248@gmail.com>
2020-07-09 13:37:33 +0530

    Added decription

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,74 +1,78 @@
-import React from 'react'
-import { useRecoilValue } from 'recoil'
-import { userAtom } from '../global/gloablState'
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { userAtom } from "../global/gloablState";
 
-const Home = ({history}) => {
+const Home = ({ history }) => {
+  const user = useRecoilValue(userAtom);
 
-    const user = useRecoilValue(userAtom)
-
-    const handleCTA = () => {
-        if(user){
-            history.push('/dashboard')
-        }else{
-            history.push('/signup')
-        }
+  const handleCTA = () => {
+    if (user) {
+      history.push("/dashboard");
+    } else {
+      history.push("/signup");
     }
+  };
 
-    return (
-        <div className="Home">
-            <div className="header">
-
-                <div className="left">
-                    <h1>Welcome to stockify</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta necessitatibus asperiores fuga iste, maiores nisi veritatis fugiat sunt mollitia dolorem.</p>
-                    <button onClick={() => handleCTA()} className="cta rev">{user ? "Go to dashboard" : "Get Started"}</button>
-                </div>
-
-                <div className="right">
-                    <img src={require("../assets/home_back.png")} alt="" />
-                </div>
-            </div>
-            <div className="divider"></div>
-            <div className="main-body">
-                <div className="features">
-                    <h1>Features</h1>
-                    <div className="items">
-                        <div className="item">
-                            <img src="https://via.placeholder.com/300" alt="" />
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, enim.</p>
-                        </div>
-                        <div className="item">
-                            <img src="https://via.placeholder.com/300" alt="" />
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, enim.</p>
-                        </div>
-                        <div className="item">
-                            <img src="https://via.placeholder.com/300" alt="" />
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, enim.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="about-us">
-                    <h1>About Us</h1>
-                    <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit exercitationem quibusdam sapiente sint dolor doloremque cum incidunt iusto, quod eveniet deleniti explicabo repellendus earum rem voluptates ipsam officia magni perspiciatis.</p>
-                    <div className="items">
-                        <div className="item">
-                            <img src="https://via.placeholder.com/300" alt="" />
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, enim.</p>
-                        </div>
-                        <div className="item">
-                            <img src="https://via.placeholder.com/300" alt="" />
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, enim.</p>
-                        </div>
-                        <div className="item">
-                            <img src="https://via.placeholder.com/300" alt="" />
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, enim.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="Home">
+      <div className="header">
+        <div className="left">
+          <h1>Welcome to stockify</h1>
+          <p>
+            Store your warehouse stocks and get an all-automated stockholder
+            registry, with complete security. The Stockify gives you a platform
+            where you can interact and manage your stocks.
+          </p>
+          <button onClick={() => handleCTA()} className="cta rev">
+            {user ? "Go to dashboard" : "Get Started"}
+          </button>
         </div>
-    )
-}
 
-export default Home
+        <div className="right">
+          <img src={require("../assets/home_back.png")} alt="" />
+        </div>
+      </div>
+      <div className="divider"></div>
+      <div className="main-body">
+        <div className="features">
+          <h1>Features</h1>
+          <div className="items">
+            <div className="item">
+              <img src="https://via.placeholder.com/300" alt="" />
+              <p>Advance CRUD operations with our app</p>
+            </div>
+            <div className="item">
+              <img src="https://via.placeholder.com/300" alt="" />
+              <p>Security kept in mind</p>
+            </div>
+            <div className="item">
+              <img src="https://via.placeholder.com/300" alt="" />
+              <p>Simple Creative UI/UX</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="about-us">
+          <h1>About Us</h1>
+          <p className="desc">Members of Team WebMasters</p>
+          <div className="items">
+            <div className="item">
+              <img src="https://via.placeholder.com/300" alt="" />
+              <p>Aman Jagdev</p>
+            </div>
+            <div className="item">
+              <img src="https://via.placeholder.com/300" alt="" />
+              <p>Kartik</p>
+            </div>
+            <div className="item">
+              <img src="https://via.placeholder.com/300" alt="" />
+              <p>Gautam Arora</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
