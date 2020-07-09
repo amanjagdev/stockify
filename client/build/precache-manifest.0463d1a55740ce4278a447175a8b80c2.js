bf743bb74dd6294bb9bee03f3781e386c1395aa0
amanjagdev <amankumarjagdev@gmail.com>
2020-07-09 11:55:36 +0530

    updated build folder

---
+++ client/build/precache-manifest.0463d1a55740ce4278a447175a8b80c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5900d632ddfafd8859af0fa4dba17503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ae52b4f9d01a7cc9a4",
+    "url": "/static/css/main.900be213.chunk.css"
+  },
+  {
+    "revision": "838bf2ec39b57b63864d",
+    "url": "/static/js/2.6dde30a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6dde30a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ae52b4f9d01a7cc9a4",
+    "url": "/static/js/main.d5b9bf32.chunk.js"
+  },
+  {
+    "revision": "c93cbfba791a93849ba0",
+    "url": "/static/js/runtime-main.6c33b0db.js"
+  },
+  {
+    "revision": "2c66385e660393281661a3675e493b8e",
+    "url": "/static/media/art.2c66385e.png"
+  },
+  {
+    "revision": "715ff8425ed065072afcd44acf1ae7cf",
+    "url": "/static/media/home_back.715ff842.png"
+  }
+]);
