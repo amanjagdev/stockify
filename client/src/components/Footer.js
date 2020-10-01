5ab3c5040560f1730d6b07499dac79af8f56b8d2
Jaidev Das <jaidevkr2@gmail.com>
2020-10-01 17:00:16 +0530

    added sticky github links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className="Footer">
-            <div className="text">Made By Webmasters &#169; 2020</div>
-        </div>
-    )
-}
+  return (
+    <div className="Footer">
+      <div className="text">Made By Webmasters &#169; 2020</div>
+      <div className="github-links">
+        <a target="_blank" href="https://github.com/amanjagdev/stockify/fork">
+          Fork us on
+        </a>
+        <a
+          id="github-icon"
+          target="_blank"
+          href="https://github.com/amanjagdev/stockify"
+        >
+          <i id="github-icon" class="im im-github"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
