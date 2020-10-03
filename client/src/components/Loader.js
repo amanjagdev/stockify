a356ed18162a9520fb70073f6738d01569bf8ad3
sanchitgupta001 <sanchit.gupta@sprinklr.com>
2020-10-03 21:59:48 +0530

    feat: Add loading handling for ActionButton in SignUp and Login Page

---
+++ client/src/components/Loader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classnames from 'classnames';
+
+function Loader({ className, style }) {
+  return <div className={className} style={style}></div>
+}
+
+export default Loader;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,63 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState, memo } from 'react';
 import { useRecoilState } from 'recoil';
-import Axios from 'axios'
+import Axios from 'axios';
 
 import { userAtom } from '../global/gloablState';
 
+// components
+import ActionButton from '../components/ActionButton';
+
 const Login = ({ history }) => {
+  const [email, setEmail] = useState('');
+  const [user, setUser] = useRecoilState(userAtom);
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState(null);
+  const [processing, setProcessing] = useState(false);
 
-    const [email, setEmail] = useState("")
-    const [user, setUser] = useRecoilState(userAtom)
-    const [password, setPassword] = useState("")
-    const [errors, setErrors] = useState(null)
+  const handleLogin = () => {
+    setProcessing(true);
+    Axios.post(`${process.env.REACT_APP_API_URL}/api/signin`, {
+      email,
+      password,
+    })
+      .then((res) => {
+        if (res.data.error) {
+          setErrors([{ msg: res.data.error }]);
+        } else {
+          localStorage.setItem('user', JSON.stringify(res.data));
+          setUser(res.data);
+          history.push('/dashboard');
+        }
+      })
+      .catch((err) => {
+        if (Array.isArray(err.response.data.errors)) {
+          setErrors(err.response.data.errors);
+        } else {
+          setErrors([{ msg: err.response.data.error }]);
+        }
+      })
+      .finally(() => setProcessing(false));
+  };
 
-    const handleLogin = () => {
-        Axios.post(`${process.env.REACT_APP_API_URL}/api/signin`, { email, password })
-            .then(res => {
-                if(res.data.error){
-                    setErrors([{msg: res.data.error}]);
-                }else{
-                    localStorage.setItem('user', JSON.stringify(res.data))
-                    setUser(res.data);
-                    history.push('/dashboard')
-                }
-            })
-            .catch(err => {
-                if (Array.isArray(err.response.data.errors)) {
-                    setErrors(err.response.data.errors);
-                } else {
-                    setErrors([{msg: err.response.data.error}]);
-                }
-            });
-    }
+  return (
+    <div className='Login'>
+      <div className='left-bar'>
+        <img src={require('../assets/logo.png')} alt='logo' className='logo' />
+        <img className='art' src={require('../assets/art.png')} alt='' />
+      </div>
+      <div className='main-login'>
+        <h1>LOGIN</h1>
+        <div className='login-form'>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='text'
+            id='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-    return (
-        <div className="Login">
-            <div className="left-bar">
-                <img src={require("../assets/logo.png")} alt="logo" className="logo" />
-                <img className="art" src={require("../assets/art.png")} alt="" />
-            </div>
-            <div className="main-login">
-                <h1>LOGIN</h1>
-                <div className="login-form">
-
-                    <label htmlFor="email">Email</label>
-                    <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-
-                    <label htmlFor="pass">Password</label>
-                    <input type="password" id="pass" value={password} onChange={(e) => setPassword(e.target.value)} />
-
-                </div>
-                <button className="login rev" onClick={() => handleLogin()}>
-                    Log In
-                </button>
-                {
-                    errors && errors.map(({ msg }, index) => <div key={index} className="error">{msg}</div>)
-                }
-            </div>
-
+          <label htmlFor='pass'>Password</label>
+          <input
+            type='password'
+            id='pass'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-    )
-}
+        <ActionButton
+          className='login rev'
+          onClick={() => handleLogin()}
+          label='Log In'
+          loading={processing}
+        />
+        {errors &&
+          errors.map(({ msg }, index) => (
+            <div key={index} className='error'>
+              {msg}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default memo(Login);
